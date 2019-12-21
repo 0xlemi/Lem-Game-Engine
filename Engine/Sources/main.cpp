bcbd198c6780701b2d36bd5954c51fd11b7a08af
Luis Espinosa <lem.espinosa.m@gmail.com>
2019-12-21 14:38:36 -0700

    Hello Rectangle: create first rectangle

--- Engine/Sources/main.cpp
+++ Engine/Sources/main.cpp
@@ -113,26 +113,34 @@ int main(int argc, char * argv[]) {
     // -------------------------
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
     };
 
-    unsigned int VBO;
+    unsigned indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
-    unsigned int VAO;
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
     glBindVertexArray(0);
 
     
@@ -150,7 +158,8 @@ int main(int argc, char * argv[]) {
         // Draw our first tringle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
