129606285a6d3db17e464850f3ed31b8ca100712
Luis Espinosa <lem.espinosa.m@gmail.com>
2019-12-25 18:48:04 -0700

    Exercice: draw 2 triangles

--- Engine/Sources/main.cpp
+++ Engine/Sources/main.cpp
@@ -112,17 +112,25 @@ int main(int argc, char * argv[]) {
     // Build Object
     // -------------------------
 
+    // float vertices[] = {
+    //      0.5f,  0.5f, 0.0f,
+    //      0.5f, -0.5f, 0.0f,
+    //     -0.5f, -0.5f, 0.0f,
+    //     -0.5f,  0.5f, 0.0f,
+    // };
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f,
+        -0.50f,  0.25f, 0.0f,
+        -0.75f, -0.25f, 0.0f,
+        -0.25f, -0.25f, 0.0f,
+         0.50f,  0.25f, 0.0f,
+         0.75f, -0.25f, 0.0f,
+         0.25f, -0.25f, 0.0f,
     };
 
-    unsigned indices[] = {
-        0, 1, 3,
-        1, 2, 3
-    };
+    // unsigned indices[] = {
+    //     0, 1, 3,
+    //     1, 2, 3
+    // };
 
     unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
@@ -134,14 +142,16 @@ int main(int argc, char * argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     
     // -------------------------
@@ -158,7 +168,8 @@ int main(int argc, char * argv[]) {
         // Draw our first tringle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // Flip Buffers and Draw
